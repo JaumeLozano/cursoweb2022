c4243bf159cf8bfc049b2df635b8cfd9ccd52da3
JaumeLozano <lozanojaume@gmail.com>
2022-05-20 18:27:10 +0200

    Les funcions y tot el tema del 20-05

---
+++ PHP/11.ejemplo_funcion.php
@@ -0,0 +1,19 @@
+<?php 
+
+// function calcular_area_triangulo($base, $altura){
+	// $resultado = ($base * $altura) / 2;
+	// return $resultado;
+// }
+
+// $area_triangulo = calcular_area_triangulo(10, 10);
+
+//echo 'El triangulo tiene un area de ' . $area_triangulo . ' metros cuadrados';
+
+//EJERCICIO: CREAR UNA FUNCIÓN PARA CALCULAR EL AREA DE UN HEXÁGONO DE FORMULA = (PERÍMETRO * APOTEMA)/2 Y EL PERÍMETRO ES IGUAL A UN LADO *6
+
+function calcular_area_hexagono($perimetro, $apotema){
+	$resultado = ($perimetro * $apotema) / 2;
+	return $resultadoareahexagono;
+}
+&area_triangulo = calcular_area_triangulo(10, 10);
+?>

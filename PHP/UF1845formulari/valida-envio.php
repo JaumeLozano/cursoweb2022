c6fab4ba05c77da3c17b0b6ead647338c5a1d278
JaumeLozano <lozanojaume@gmail.com>
2022-05-31 17:22:01 +0200

    Classe 31-05-2022

--- PHP/UF1845formulari/valida-envio.php
+++ PHP/UF1845formulari/valida-envio.php
@@ -1,5 +1,7 @@
 <?php
-
+if(!$_GET && !$_POST){ //Podríamos poner también &&(!POST)) para evitar que no se redireccione en caso que sea  otra metodología. En las wbs se utilizará un único documento en toda la web 
+ header('Location: http://localhost/cursoweb2022/PHP/UF1845formulari/formulario.php'); //Carpeta y archivo para rellenar el form correcto
+}
 // print_r($_SERVER);
 #1. REVISAR DE QUE FORMA SE ENVIAN LOS DATOS
 // if ($_SERVER['REQUEST_METHOD'] == 'GET') {
@@ -8,20 +10,20 @@
 //     echo 'Se ha enviado por POST';
 // }
 
-#2. REVISAR SI SE HA ENVIADO EL FORMULARIO COMPROBANDO SUBMIT
-// if (isset($_POST['submit'])) {
-//     echo 'Se han enviado los datos correctamente <br>';
-//     print_r($_POST['submit']);
+// #2. REVISAR SI SE HA ENVIADO EL FORMULARIO COMPROBANDO SUBMIT
+// if (isset($_GET['submit'])) {
+//     echo 'Se han enviado los datos correctamente por el método GET <br>';
+//     print_r($_GET['submit']);
 //     echo '<br>';
-//     print_r($_POST);
+//     print_r($_GET);
 // }
-// if (isset($_POST['submit2'])) {
-//     echo 'Se han enviado los datos correctamente del segundo formulario ';
-//     print_r($_POST['submit2']);
+// if (isset($_GET['submit2'])) {
+//     echo 'Se han enviado los datos correctamente del segundo formulario por el método GET. ';
+//     print_r($_GET['submit2']);
 // }
 
 #3. EJERCICIO REVISAR SI SE HA ENVIADO POR POST O GET Y ADEMÁS EN EL FORMULARIO COMPROBAMOS QUE SUBMIT
-$metodo= $_SERVER['REQUEST_METHOD'];
+$metodo= $_SERVER['REQUEST_METHOD']; //antes de nada ya pregunta el tipo que es. Te solicita el methodo. 
 if ($metodo == 'GET') {//Enviado mediante GET
     if (isset($_GET['submit'])) {
         echo 'Se han enviado los datos correctamente en el formulario 1, mediante GET <br>';
@@ -43,7 +45,7 @@ if ($metodo == 'GET') {//Enviado mediante GET
         print_r($_POST['submit2']);
     }
     echo print_r($_GET); //VACIO
-    echo print_r($_POST);
+    echo print_r($_POST); //es el que devuelve el valor de 1. 
 }
 
 ?>

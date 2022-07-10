4e923882700e51fdc8b05e759ff5534554e2d28c
JaumeLozano <105653692+JaumeLozano@users.noreply.github.com>
2022-07-10 21:25:42 +0200

    Canvis pre-examen

---
+++ PHP/Session/ejemplo3trabajo.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+echo 'Bienvenido a tu perfil. Puedes navegar por la plataforma<br />';
+echo date('H:i:s d m Y', $_SESSION['El momento concreto']); 
+?> 
+
+echo $_SESSION['color'] . '<br>';  // verde
+echo $_SESSION['animal']. '<br>'; // gato
+echo date('H:i:s d m Y', $_SESSION['instante']); //se le está dando la hora de inicio, cuando ha empezado esa complicación del código PHP. o por emplo cuando cadurá la sesión 
+
+// Puede ser conveniente usar el SID aquí, como hicimos en pagina1.php
+echo '<br /><a href="ejemplo1.php">Ejemplo 1</a>';
+?> 

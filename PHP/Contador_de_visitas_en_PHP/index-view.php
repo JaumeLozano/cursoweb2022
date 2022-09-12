e30c007da5ac2d8c7971b1688ea6851ccd228b17
JaumeLozano <lozanojaume@gmail.com>
2022-09-12 19:58:37 +0200

    Apunts examen

---
+++ PHP/Contador_de_visitas_en_PHP/index-view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Contador de Visitas</title>
+	<link rel="stylesheet" href="estilos.css">
+	<link href='https://fonts.googleapis.com/css?family=Oswald:700,400,300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<h1>Contador de Visitas</h1>
+	<div class="visitantes">
+		<p class="numero"><?php echo contar_usuarios(); ?></p>
+		<p class="texto">Visitas</p>
+		<button onclick="reinicio_contador()">Reiniciar contador</button>
+	</div>
+</body>
+</html>
+<?php
+function reinicio(){
+	$archivo = 'contador.txt';
+
+	$reinicio = file_get_contents($archivo, 0);
+	file_put_contents($archivo, $reinicio);
+	return $reinicio; 
+}
+?>
